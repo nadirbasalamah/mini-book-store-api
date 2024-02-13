fd0eb213fbbb0d5456c8a898cf1dc80e47267035
nadirbasalamah <nadir008basalamah@gmail.com>
2024-02-13 08:19:42 +0700

    feat(cors): add cors middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { PORT, MONGO_URI } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
 import router from "./routes/routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use("/api/v1", router);
 
