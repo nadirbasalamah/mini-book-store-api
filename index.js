2b6aa0511501a084358f0e714687f411aef9e9c8
nadirbasalamah <nadir008basalamah@gmail.com>
2024-02-13 13:35:59 +0700

    fix(cors): fix for deployment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mini-book-store-fe.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
